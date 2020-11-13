07068b5ac66397d90488c9d53a12b38edf9a4f3f
Aleko Georgiev <61247544+po1nt3r@users.noreply.github.com>
2020-11-13 14:04:43 +0200

    classwork3

---
+++ classwork3/classwork3.py
@@ -0,0 +1,45 @@
+import math
+
+#задача 1
+'''
+a = input('Enter a string: ')
+vowels = ['A','a','E','e','I','i','O','o','U','u']
+a = list(a)
+
+for i in vowels:
+    for j in range(len(a)):
+        if i == a[j]:
+            for k in range(j, 3+j, 1):
+                a.insert(k, i)
+
+a = ''.join(a)
+
+print(a)
+'''
+#задача 2
+'''
+num = int(input())
+count = 0
+
+while num != 0:
+    num //= 10
+    count += 1
+
+print('Lenght ->', count)
+'''
+#задача 3
+'''
+num = int(input("Enter a number: "))
+num1 = num
+count = 0
+while num >= 2:
+    num = math.sqrt(num)
+    count += 1
+
+print(num1,'->',count)
+'''
+#задача 4
+#finish later
+num = int(input("Enter a number: "))
+prime = True
+count = 0
