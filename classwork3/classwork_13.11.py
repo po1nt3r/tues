21b063f5975fe4bf4b3d1e0f7fef03b355af76b6
Aleko Georgiev <61247544+po1nt3r@users.noreply.github.com>
2020-11-13 16:37:58 +0200

    fix

--- classwork3/classwork_13.11.py
+++ classwork3/classwork_13.11.py
@@ -5,15 +5,18 @@ import math
 a = input('Enter a string: ')
 vowels = ['A','a','E','e','I','i','O','o','U','u']
 a = list(a)
+j = 0
 
-for i in vowels:
-    for j in range(len(a)):
-        if i == a[j]:
-            for k in range(j, 3+j, 1):
+while j < len(a):
+    for i in vowels:
+        if a[j] == i:
+            for k in range(j, j+3):
                 a.insert(k, i)
-
+                print('lenth of a: ' , len(a))
+            j += 3
+            print(j)
+    j += 1
 a = ''.join(a)
-
 print(a)
 '''
 #задача 2
