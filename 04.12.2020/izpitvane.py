79af8fc6cd23580498f619d601a779e2c4e2bbe9
Aleko Georgiev <61247544+po1nt3r@users.noreply.github.com>
2020-12-04 13:04:58 +0200

    izpitvane

---
+++ 04.12.2020/izpitvane.py
@@ -0,0 +1,30 @@
+a = input("Enter a number for a: ")
+b = input("Enter a number for b: ")
+c = input("Enter a number for c: ")
+
+a = list(a)
+b = list(b)
+c = list(c)
+
+def is_equal(a, b, c):
+    num = str(int(a[len(a) - 1]) * int(b[len(b) - 1]))
+    num = list(num)
+
+    if int(num[len(num)-1]) == int(c[len(c)-1]):
+        return True
+    else: 
+        return False
+
+print(is_equal(a, b, c))
+
+
+limit = int(input("Ogranichenie: "))
+distance = int(input("Razstoqnie koeto trqbva da bude izminato: "))
+
+def speeding(limit, distance):
+    time1 = distance/(limit)
+    time2 = distance/(limit + 15)
+    
+    return (time1 - time2)*60
+
+print("%.2f minutes"%(speeding(limit, distance)))
