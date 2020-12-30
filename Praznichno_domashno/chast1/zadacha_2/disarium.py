5948ac73984fe62980d913c87f6980546b48bd04
Aleko Georgiev <61247544+po1nt3r@users.noreply.github.com>
2020-12-30 22:47:32 +0200

    Update disarium.py

--- Praznichno_domashno/chast1/zadacha_2/disarium.py
+++ Praznichno_domashno/chast1/zadacha_2/disarium.py
@@ -1,14 +1,14 @@
 def disarium_func(n):
     pos = len(n)
-    sum = 0
+    sum1 = 0
     n1 = int(n)
     
     while n1 != 0:
-        sum += (n1%10)**pos
+        sum1+= (n1%10)**pos
         n1 //= 10
         pos -= 1
     
-    if sum == int(n):
+    if sum1 == int(n):
         return True
     else:
         return False 
