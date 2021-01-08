27eb51158576a76d89187ec7c435b072097e117c
Aleko Georgiev <61247544+po1nt3r@users.noreply.github.com>
2021-01-08 13:51:51 +0200

    Update hanged-man.py

--- classwork/hanged-man.py
+++ classwork/hanged-man.py
@@ -1,5 +1,10 @@
 from os import  system
 
+def check(string, reveal, char):
+    for i in range(len(string)):
+        if char == string[i]:
+            reveal[i] = char
+
 lives = 6
 string = list(input("Enter a word: "))
 reveal = ["_" for i in range(len(string))]
@@ -10,7 +15,7 @@ char = ''
 while lives >= 0:
     char = input("Enter a charachter: ")
     if char in string:
-        reveal[string.index(char)] = char
+        check(string, reveal, char)
     else:
         lives -= 1
     print(f"lives: {lives}, string: "+''.join(reveal))
