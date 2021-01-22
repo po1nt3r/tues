ae4aaaaedbd8f63db57c5ce27d1aaec7cfcee41e
Aleko Georgiev <61247544+po1nt3r@users.noreply.github.com>
2021-01-22 13:21:39 +0200

    Update calculate.py

--- izpitvane2/pay/calculate.py
+++ izpitvane2/pay/calculate.py
@@ -2,8 +2,8 @@ def calculate(days):
     if days < 14:
         return 200*days
     elif days >= 14 and days < 30:
-        return (200*days) + 50*(days - 14) 
+        return (200*days) + 50*days
     elif days >= 30 and days < 60:
-        return (200*days) + 80*(days - 14)
+        return (200*days) + 80*days
     else:
-        return (200*days) + 100*(days - 14)
+        return (200*days) + 100*days
