3373f2d2e77f61a3986c0fc4c862f64c36cd51e8
Aleko Georgiev <61247544+po1nt3r@users.noreply.github.com>
2021-01-22 15:02:14 +0200

    Update client.py

--- client1/client.py
+++ client1/client.py
@@ -1,14 +1,25 @@
 import socket
+import pickle
 
 while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("34.77.60.185", 8080))
-    while True:
-        userInput = input("Enter time, weather or air quality")
-        s.send(bytes(userInput, "utf-8"))
-        print(s.recv(1024).decode("utf-8"))
-        if userInput == "bye":
-            s.close()
-            break
-    if input("Type 1 to reestablish the connection: ") != '1':
+    
+    userInput = input("Enter 'time', 'weather' or 'airquality': ")
+    command = pickle.dumps(userInput)
+    s.send(command)
+    data = s.recv(1024)
+
+    #Господине, pickle.dump()-нали сте информацията, която се връща 2 пъти
+    decode_data1 = pickle.loads(data)
+    decode_data2 = pickle.loads(decode_data1)
+
+    s.close()
+
+    print(decode_data2)
+
+    if input("Type 1 to reestablish the connection: ") != 1:
         break
+
+
+    
