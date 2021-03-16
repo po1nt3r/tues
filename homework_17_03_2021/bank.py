8f1a61d1865947a136b6bdaa88dcd358b46b20a6
Aleko Georgiev <61247544+po1nt3r@users.noreply.github.com>
2021-03-16 22:36:49 +0200

    bug fixes

--- homework_17_03_2021/bank.py
+++ homework_17_03_2021/bank.py
@@ -30,10 +30,9 @@ class Client():
     def __init__(self):
         Client.num_of_clients += 1 
         if os.path.exists(PATH):
-            with open(PATH, "r") as fr:
-                self.account_dict = yaml.full_load(fr)
-                self.bank_accounts = []
-                self.num_accounts = 0
+            self.account_dict = self.read_file()
+            self.bank_accounts = []
+            self.num_accounts = 0
             if len(self.account_dict) >= Client.num_of_clients:
                 self.id = list(self.account_dict.keys())[Client.num_of_clients-1]
                 self.num_accounts = len(self.account_dict[self.id])
@@ -43,7 +42,7 @@ class Client():
                 for _ in range(0, 8):
                     self.id += random.choice(alphabet)
                 self.add_bank_account(int(input("Enter balance: ")))
-                self.account_dict.update({self.id : i for i in self.bank_accounts})
+                self.account_dict.update({self.id : [i for i in self.bank_accounts]})
                 with open("accounts.yaml", "a") as fw:
                     yaml.dump({self.id : [i for i in self.bank_accounts]}, fw)
 
@@ -54,11 +53,17 @@ class Client():
             self.id = ""
             for _ in range(0, 8):
                 self.id += random.choice(alphabet)            
-            self.account_dict = {self.id:self.bank_accounts[0]}
-            with open(PATH, "w") as fw:
-                yaml.dump(self.account_dict, fw)
+            self.account_dict = {self.id:[i for i in self.bank_accounts]}
+            print(self.account_dict)
+            self.write_file()
             
-
+    def write_file(self):
+        with open(PATH, "w") as fw:
+            yaml.dump(self.account_dict, fw)
+    
+    def read_file(self):
+        with open(PATH, "r") as fr:
+            return yaml.full_load(fr)
 
     def add_bank_account(self, amount, modify = False):
         self.num_accounts += 1
@@ -66,14 +71,9 @@ class Client():
         self.bank_accounts.append({f"Account {self.num_accounts}": b.balance})
 
         if modify:
-            with open("accounts.yaml", "r") as fr:
-                self.account_dict = yaml.load(fr, Loader=yaml.FullLoader)
-                # print(self.account_dict)
-                self.account_dict[self.id] = self.bank_accounts
-
-                # print(self.account_dict)
-                with open("accounts.yaml", "w") as fw:
-                    yaml.dump(self.account_dict, fw)
+            self.account_dict = self.read_file()
+            self.account_dict[self.id] = self.bank_accounts
+            self.write_file()
 
     def print_acc(self):    
         for i in self.bank_accounts:
@@ -82,13 +82,10 @@ class Client():
     def deposit(self, amount):
         if not self.bank_accounts:
             raise ValueError("No bank accounts")
-        print("banana")
         self.print_acc()
         acc_num = int(input("Enter the number of the account to choose it: "))
         self.account_dict[self.id][acc_num-1][f"Account {acc_num}"] += amount
-        with open("accounts.yaml", "w") as fw:
-            yaml.dump(self.account_dict, fw)
-        
+        self.write_file()
 
     def withdraw(self, amount):
         if not self.bank_accounts:
@@ -96,27 +93,23 @@ class Client():
         self.print_acc()
         acc_num = int(input("Enter the number of the account to choose it: "))
         self.account_dict[self.id][acc_num-1][f"Account {acc_num}"] -= amount
-        with open("accounts.yaml", "w") as fw:
-            yaml.dump(self.account_dict, fw)
+        self.write_file()
 
     def transfer(self, client, amount, operation="deposit"):
         if not isinstance(client, Client):
             print("Not a client")
             return
         if operation == "deposit":
-            # add reading and writing
             client.withdraw(amount)
-            with open("accounts.yaml", "r") as fr:
-                self.account_dict = yaml.full_load(fr)
+            self.account_dict = self.read_file()
             self.deposit(amount)
         else:
             client.deposit(amount)
-            with open("accounts.yaml", "r") as fr:
-                self.account_dict = yaml.full_load(fr)
+            self.account_dict = self.read_file()
             self.withdraw(amount)
 
 client1 = Client()
-print(client1.account_dict)
-client2 = Client()
-client2.transfer(client1, 250, "deposit")
 
+client2 = Client()
+client3 = Client()
+client1.transfer(client2, 250)
