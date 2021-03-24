8a10a0325a1e6beaa1ad5bbff9621ad2eb2a4ee6
Aleko Georgiev <61247544+po1nt3r@users.noreply.github.com>
2021-03-24 14:39:35 +0200

    fixed

--- classwork_24_03_2021/diary.py
+++ classwork_24_03_2021/diary.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 class Uchenik:
     def __init__(self, number_in_class, name, grades = None):
@@ -19,9 +20,14 @@ class Uchenik:
 
 class Dnevnik:
     def __init__(self, num_of_students):
-        self.students = self.enter_students(num_of_students)
-        self.num_of_students = num_of_students
         self.diary_dict = {}
+        if os.path.exists("diary.json"):
+            self.students = self.read_diary()
+            self.num_of_students = len(self.students)
+        else:
+            self.students = self.enter_students(num_of_students)
+            self.num_of_students = num_of_students
+            
     
     def enter_students(self, num_of_students):
         students = []
@@ -34,21 +40,24 @@ class Dnevnik:
     def write_diary(self):
         self.diary_dict = {}
         for i in range(self.num_of_students):
-            diary_dict[self.students[i].number_in_class] = dict({"name": self.students[i].name, 
+            self.diary_dict[self.students[i].number_in_class] = dict({"name": self.students[i].name, 
             "grades": self.students[i].grades})
         
         with open("diary.json", "w") as fw:
-            json.dump(diary_dict, fw, indent = 4)
+            json.dump(self.diary_dict, fw, indent = 4)
     
     def read_diary(self):
         with open("diary.json", "r") as fr:
             self.diary_dict = json.load(fr)
-        numbers = list(self.diary.keys())
-        values = list(self.diary.values())
-        for i in range(self.num_of_students):
+        num_of_students = len(self.diary_dict)
+        numbers = list(self.diary_dict.keys())
+        values = list(self.diary_dict.values())
+        students = []
+        for i in range(num_of_students):
             a = Uchenik(numbers[i], values[i]["name"], values[i]["grades"])
-            self.students.append(a)
+            students.append(a)
+        
+        return students
 
 diary = Dnevnik(1)
 diary.write_diary()
-
