61ad2bd567ff091c49c13c0182f3eba0966a8fd0
Aleko Georgiev <61247544+po1nt3r@users.noreply.github.com>
2021-04-01 23:47:16 +0300

    bad code

--- homework_17_03_2021/bank.py
+++ homework_17_03_2021/bank.py
@@ -1,9 +1,30 @@
 import random
 import os
 import yaml
+import argparse
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-PATH = "accounts.yaml"
+PATH = None
+
+def new_client(client_list):
+    if isinstance(client_list, list):
+        client_list.append(Client())
+    else:
+        raise ValueError("Object is not a list")
+
+def clients_for_transaction(client_list, transaction_properties):
+    transaction_pr = ['', '', int(transaction_properties[2])]
+    for i in client_list:
+        if i.id == transaction_properties[0]:
+            transaction_pr[0] = i
+        elif i.id == transaction_properties[1]:
+            transaction_pr[1] = i
+    
+    if "" in transaction_pr:
+        raise ValueError("Client id does not exist")          
+    
+    return transaction_pr      
+            
 
 class BankAccount:
     def __init__(self, balance):
@@ -107,9 +128,33 @@ class Client():
             client.deposit(amount)
             self.account_dict = self.read_file()
             self.withdraw(amount)
+            
+            
+parser = argparse.ArgumentParser()
 
-client1 = Client()
+parser.add_argument("-f", action="store", dest="filename", required=True,
+                    help="Path to yaml file.")
+parser.add_argument("-ac", action="store", dest="add_clients", type=int, required=True,
+                    help="Number of clients to be added.")
+parser.add_argument("-t", action="store", dest="transaction_type", help="Deposit or withdraw",
+                    default="deposit")
+parser.add_argument("-op", action="store", dest="transaction_properties", nargs=3, required=True,
+                    help="id of client to withdraw/deposit | id of client to withdraw/deposit | amount")
 
-client2 = Client()
-client3 = Client()
-client1.transfer(client2, 250)
+options = parser.parse_args()
+PATH = options.filename
+client_list = []
+
+for _ in range(options.add_clients):
+    new_client(client_list)
+
+for i in range(options.add_clients):
+    print(client_list[i].id)
+    client_list[i].print_acc() 
+
+transaction_pr = clients_for_transaction(client_list, options.transaction_properties)
+transaction_pr[0].transfer(transaction_pr[1], transaction_pr[2])
+
+
+
+# client1.transfer(client2, 250)
