2bccb199dc7c46065c589fc09283c55e077ae03a
Aleko Georgiev <61247544+po1nt3r@users.noreply.github.com>
2021-04-21 14:00:39 +0300

    bad code

---
+++ classwork_21_04_2021/papki.py
@@ -0,0 +1,17 @@
+import os
+import random
+
+
+num_of_files = int(input("Enter number of files: "))
+
+os.mkdir("folders")
+
+    
+for i in range(num_of_files):
+    os.mkdir(f"folders/folder{i}")
+    random_int = random.randint(0, 10)
+    print(random_int)
+    
+    for j in range(random_int):
+        with open(f"folders/folder{i}/file{j}.txt", "w") as fw:
+            fw.write(str(j))
