06e6d92cb44ee85dbc9d6768bec4d3d465030375
Aleko Georgiev <61247544+AlekoGeorgiev@users.noreply.github.com>
2021-04-28 13:40:13 +0300

    Create izpitvane.py

---
+++ izpitvane_28_04/izpitvane.py
@@ -0,0 +1,27 @@
+class Files:
+    def __init__(self, file_list):
+        self.file_list = file_list
+        
+    def add_file(self, new_file):
+        self.file_list.append(new_file)
+    
+    def read_all(self):
+        for i in self.file_list:
+            with open(i, "r") as fr:
+                print(fr.read())
+    
+    def count_all(self, word):
+        count = 0
+        for i in self.file_list:
+            with open(i, "r") as fr:
+                text = fr.read()
+                text = text.split(" ")
+                count += text.count(word)
+        
+        return count
+                
+
+files = Files(["file1.txt"])
+files.add_file("file2.txt")
+files.read_all()
+print("Number of occurances: ", files.count_all(input("Enter a word: ")))
